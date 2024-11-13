9bee0218ef9a83aa21273190a49b7ed0e78ae2d6
as4792 <as4792@njit.edu>
2024-11-13 15:12:31 -0500

    answer all lab questions

---
+++ script.js
@@ -0,0 +1,126 @@
+
+//Q1
+
+let Firstname= "Ana";
+let Age= "20";
+
+console.log(Firstname);
+console.log(Age);
+
+
+//Q2
+
+let stringVar="STRING";
+
+console.log(stringVar);
+
+let number1= 22;
+
+console.log(numberVar);
+
+let booleanVar= true;
+
+console.log(booleanVar);
+
+let undefinedVar;
+
+console.log(undefinedVar);
+
+//Q3
+
+let RollNumber= "15";
+
+if (RollNumber > 10){
+    console.log("The RollNumber is greater than 10");
+}
+
+
+//Q4
+
+let isMember = true;
+if (isMember){
+    console.log("Member discount applied.");
+}
+
+//Q5
+
+let startValue = 50;
+if (startValue > 0){
+    console.log("positive");
+}
+else if (startValue < 0){
+    console.log("negative");
+}
+else{
+    console.log("zero");
+}
+
+//Q6
+
+let a = 1;
+let b = 2;
+
+console.log(a =b);
+console.log(a===b);
+console.log(a!=b);
+console.log(a>b);
+console.log(a<b);
+console.log(a>=b);
+console.log(a<=b);
+
+//Q7
+
+let firstName= "anacaro";
+let lastName= "fernandes";
+let age2= 19;
+console.log("Hello,"+ firstName + " " + lastName + ", you are" + age2 + "years old.");
+
+//Q8
+
+let number2 = 50;
+if (number2Var > 0 && number2Var < 100){
+    console.log("This number is within range");
+}
+
+
+//Q9
+
+let hasCar= true;
+let hasLicense= true;
+let hasInsurance= true;
+if (hasCar && hasInsurance && hasLicense){
+    console.log("You can drive");
+}
+else { 
+    console.log("Check your driving eligibiity");
+}
+
+
+//Q10
+
+let number3= 21;
+if (number3 % 2 === 0){
+    console.log("Number is even.");
+}
+else{
+    console.log("Number is odd");
+}
+
+//Q11
+let score = 85;
+if (score >= 90){
+    console.log("Grade A");
+}
+else if (score >= 80 && score < 90){
+    console.log("Grade B");
+}
+else {
+    console.log("Grade C");
+}
+
+
+
+
+
+
+
